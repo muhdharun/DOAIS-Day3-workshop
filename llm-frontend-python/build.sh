bd412d01191c1d8c192c3e0666b34b0afcc40875
muhdharun <harunabdul.harun@gmail.com>
2026-09-20 15:36:48 +0800

    triggering pipeline

--- llm-frontend-python/build.sh
+++ llm-frontend-python/build.sh
@@ -1,2 +1,2 @@
-  docker build --platform linux/arm64 -t darryl1975/llm-frontend-python:latest .        
-  docker push darryl1975/llm-frontend-python:latest 
+  docker build --platform linux/arm64 -t mdharun/llm-frontend-python:latest .
+  docker push mdharun/llm-frontend-python:latest
